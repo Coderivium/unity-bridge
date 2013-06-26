77b548f6bc214713b247a420371e7bd9f65330f2
Igor Ishchenko <igor.ishchenko@coderivium.com>
2013-06-26 13:25:23 +0300

    added documentation

---
+++ Documentation/search/classes_63.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['crunitybridge',['CRUnityBridge',['../d3/d2f/interface_c_r_unity_bridge.html',1,'']]]
+];
---
+++ Documentation/search/functions_63.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['converttochar',['convertToChar',['../da/da4/category_n_s_string_07_conversion_to_char_08.html#abfa5aa1372547da3bdb40b3b1ddd6c7b',1,'NSString(ConversionToChar)']]]
+];
